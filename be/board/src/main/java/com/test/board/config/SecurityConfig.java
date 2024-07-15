f655c0b47985930f8b2c3a3297367d85aebfd1ce
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-07-15 17:24:09 +0900

    security config 파일 설정

---
+++ be/board/src/main/java/com/test/board/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.test.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(auth -> auth.disable())
+                .formLogin(auth -> auth.disable())
+                .httpBasic(auth -> auth.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
